dd687288c40cc2d991ff45f06fb55cc04e8c6731
burak seçer <buraksecer@users.noreply.github.com>
2019-12-01 20:16:00 +0300

    NlogToMongoDbNetCore

---
+++ NlogToMongoDb/NlogToMongoDb/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLog;
+using NLog.Fluent;
+
+namespace NlogToMongoDb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        public IActionResult Index()
+        {
+            _logger.Debug("DebugLog");
+            _logger.Trace("TraceLog");
+            _logger.Error("ErrorLog");
+            _logger.Fatal("FatalLog");
+            _logger.Info("InfoLog");
+            _logger.Warn("WarnLog");
+
+            _logger.Info()
+                .Message("MessageLog")
+                .Property("testpro", "properties")
+                .Write();
+
+            return View();
+        }
+    }
+}
